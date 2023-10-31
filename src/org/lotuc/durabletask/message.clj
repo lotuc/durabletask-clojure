fa0afc823663f6a9da784012173cc9d427aeaf18
lotuc <lotu.c@outlook.com>
2023-10-31 21:49:59 +0800

    chore(proto): durabletask proto message & clj map converters

--- src/org/lotuc/durabletask/message.clj
+++ src/org/lotuc/durabletask/message.clj
@@ -8,37 +8,61 @@
 
 (declare mapper)
 
-(defn- message-names [] (str/split-lines (slurp (io/resource "durabletask_messages.txt"))))
-(def ^:private java-package "com.microsoft.durabletask.implementation.protobuf")
-(def ^:private outer-classname "OrchestratorService")
+;;; orchestrator_service.proto
+(defn- durabletask-message-names [] (str/split-lines (slurp (io/resource "durabletask_messages.txt"))))
+(def ^:private durabletask-java-package "com.microsoft.durabletask.implementation.protobuf")
+(def ^:private durabletask-java-outer-name "OrchestratorService")
 
-(defmacro import-durabletask-messages []
-  (let [classes (map (fn [n] (symbol (str java-package "."  outer-classname "$" n))) (message-names))]
-    `(import ~@classes)))
+;;; google protobuf messages used in orchestrator_service.proto
+(def ^:private protobuf-java-package "com.google.protobuf")
+(def ^:private protobuf-message-names ["Empty" "Duration" "Int32Value" "StringValue" "Timestamp"])
 
-(defmacro def-durabletask-mapper []
-  (let [classes (map (fn [n] (symbol (str outer-classname "$" n))) (message-names))]
-    `(p/defmapper ~'mapper [~@classes])))
+(defn- make-class-symbol [outer-name class-name]
+  (symbol (if outer-name (str outer-name "$" class-name) class-name)))
 
-(defn def-clj-map->proto-map [clazz-name]
-  (let [n (symbol (str "clj-map->proto-map-" clazz-name))
-        c (symbol (str "OrchestratorService$" clazz-name))]
-    `(def ~n (fn [m#] (p/clj-map->proto-map mapper ~c m#)))))
+(defn- make-fqn [package-name outer-name class-name]
+  (symbol (str package-name "." (if outer-name (str outer-name "$" class-name) class-name))))
 
-(defn def-bytes->proto-map [clazz-name]
-  (let [n (symbol (str "bytes->proto-map-" clazz-name))
-        c (symbol (str "OrchestratorService$" clazz-name))]
-    `(def ~n (fn [b#] (p/bytes->proto-map mapper ~c b#)))))
+(defmacro import-classes []
+  (let [protobuf-classes (->> protobuf-message-names
+                              (map (partial make-fqn protobuf-java-package nil)))
+        durabletask-classes (->> (durabletask-message-names)
+                                 (map (partial make-fqn durabletask-java-package durabletask-java-outer-name)))]
+    `(import ~@protobuf-classes
+             ~@durabletask-classes)))
+
+(defmacro def-mapper []
+  (let [google-protobuf-classes (->> protobuf-message-names
+                                     (map (partial make-class-symbol nil)))
+        durabletask-classes (->> (durabletask-message-names)
+                                 (map (partial make-class-symbol durabletask-java-outer-name)))]
+    `(p/defmapper ~'mapper [~@google-protobuf-classes
+                            ~@durabletask-classes])))
+
+(defn- build-convertions [outer-name clazz-name]
+  (let [clj-map->proto-map-clazz (symbol (str "clj-map->" clazz-name "-proto-map"))
+        clj-map->clazz (symbol (str "clj-map->" clazz-name))
+        bytes->proto-map-clazz (symbol (str "bytes->" clazz-name "-proto-map"))
+        clazz (make-class-symbol outer-name clazz-name)]
+    [`(def ~clj-map->proto-map-clazz (fn [m#] (p/clj-map->proto-map mapper ~clazz m#)))
+     `(def ~clj-map->clazz (fn [m#] (p/proto-map->proto (p/clj-map->proto-map mapper ~clazz m#))))
+     `(def ~bytes->proto-map-clazz (fn [b#] (p/bytes->proto-map mapper ~clazz b#)))]))
 
 (defmacro def-conversions []
-  (let [names (message-names)
-        d0 (map def-clj-map->proto-map names)
-        d1 (map def-bytes->proto-map names)]
-    `(do ~@d0 ~@d1)))
+  (let [google-protobuf-convertions (->> protobuf-message-names
+                                         (map (partial build-convertions nil))
+                                         (reduce concat))
+        durabletask-convertions (->> (durabletask-message-names)
+                                     (map (partial build-convertions durabletask-java-outer-name))
+                                     (reduce concat))]
+    `(do ~@google-protobuf-convertions
+         ~@durabletask-convertions)))
 
-(import-durabletask-messages)
-(def-durabletask-mapper)
+(import-classes)
+(def-mapper)
 (def-conversions)
+
+(def proto-empty (Empty/getDefaultInstance))
 
 (def proto->proto-map (partial p/proto->proto-map mapper))
 (def clj-map->proto-map (fn [clazz m] (p/clj-map->proto-map mapper clazz m)))
